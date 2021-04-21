71771db3283b2a73bbd828186f447f6745a916dc
Ty Poorman <git@flotts.io>
2021-04-20 23:37:49 -0400

    made some changes, added a settings file

--- main.lua
+++ main.lua
@@ -1,55 +1,64 @@
 function love.load()
-    love.graphics.setPointSize(5)
-    love.graphics.setDefaultFilter("nearest", "nearest")
+    -- editor configuration
+    settings = require("settings")
 
-    -- load from file?
-    config = {
-        scale = 1,
-        nearest = true
-    }
+    -- apply editor config as needed
+    if settings.nearest then
+        love.graphics.setDefaultFilter("nearest", "nearest")
+    end
+    love.graphics.setBackgroundColor(settings.bg)
 
+    -- animation file info, will be saved in project file
     anim = {
-        drawing = false,
-        playing = false,
-        tick = 0,
-        frame = 1,
         frames = { newframe() },
-        drawcolor = { 1, 0, 0 },
-    }
-
-    settings = {
-        speed = 10,
+        fps = 10,
         onion = false,
     }
+
+    -- editor state, could also be saved in project file
+    editor = {
+        tick = 0,
+        frame = 1,
+        playing = false,
+        drawing = false,
+        drawcolor = { 0, 0, 0 },
+        drawsize = 5
+    }
+
+    -- apply editor state as needed
+    love.graphics.setPointSize(editor.drawsize)
 end
 
 function love.update(dt)
-    anim.drawing = love.mouse.isDown(1)
+    -- this only really makes sense if the mouse is over the canvas
+    editor.drawing = love.mouse.isDown(1)
 
-    if anim.playing then
-        anim.tick = anim.tick + settings.speed * dt
-        anim.frame = math.floor(anim.tick) % #anim.frames + 1
+    if editor.playing then
+        editor.tick = editor.tick + anim.fps * dt
+        editor.frame = 1 + math.floor(editor.tick) % #anim.frames
     end
-
-    -- update mouse location on canvas???
 end
 
 function love.draw()
-    if anim.drawing then
-        -- draw to current frame canvas
-        love.graphics.setCanvas(anim.frames[anim.frame])
-        love.graphics.setColor(anim.drawcolor)
-        love.graphics.circle("fill", 0,  0, 4)
-        love.graphics.setCanvas()
-    end
-
     -- translate origin to center
     love.graphics.translate(love.graphics.getWidth() / 2, love.graphics.getHeight() / 2)
 
+    if editor.drawing then
+        -- draw to current frame canvas
+        love.graphics.setCanvas(anim.frames[editor.frame])
+        love.graphics.setColor(editor.drawcolor)
+
+        -- i still need to figure out why this works
+        local mx, my = love.graphics.inverseTransformPoint(love.mouse.getX(), love.mouse.getY())
+        love.graphics.circle("fill", mx / settings.scale - (love.graphics.getWidth() / 2) + 128, my / settings.scale - (love.graphics.getHeight() / 2) + 128, 4)
+
+        love.graphics.setCanvas()
+    end
+
     -- draw interface
     love.graphics.setColor(1, 1, 1)
-    love.graphics.print("frame: "..anim.frame.." / frames: "..#anim.frames, -200, -270)
-    if anim.playing then
+    love.graphics.print("frame: "..editor.frame.." / frames: "..#anim.frames, -256, -270)
+    if editor.playing then
         love.graphics.print("PLAY", 128, -272)
     else
         love.graphics.print("PAWS", 128, -272)
@@ -57,23 +66,23 @@ function love.draw()
 
     -- draw canvas background
     love.graphics.setColor(1, 1, 1)
-    love.graphics.rectangle("fill", 0, 0, 512, 512)
+    love.graphics.rectangle("fill", -128 * settings.scale, -128 * settings.scale, 256 * settings.scale, 256 * settings.scale)
 
     -- draw previous frame, if onion skinning is enabled
-    if settings.onion and anim.frame > 1 then
+    if anim.onion and editor.frame > 1 then
         love.graphics.setColor(0.9, 0.9, 1, 0.5)
-        love.graphics.draw(anim.frames[anim.frame - 1], 0, 0, 0, 2, 2)
+        love.graphics.draw(anim.frames[editor.frame - 1], -128 * settings.scale, -128 * settings.scale, 0, 2, 2)
     end
 
     -- draw next frame, if onion skinning is enabled
-    if settings.onion and anim.frame < #anim.frames then
+    if anim.onion and editor.frame < #anim.frames then
         love.graphics.setColor(1, 0.9, 0.9, 0.5)
-        love.graphics.draw(anim.frames[anim.frame + 1], 0, 0, 0, 2, 2)
+        love.graphics.draw(anim.frames[editor.frame + 1], -128 * settings.scale, -128 * settings.scale, 0, 2, 2)
     end
 
     -- draw current frame
     love.graphics.setColor(1, 1, 1)
-    love.graphics.draw(anim.frames[anim.frame], 0, 0, 0, 2, 2)
+    love.graphics.draw(anim.frames[editor.frame], -128 * settings.scale, -128 * settings.scale, 0, 2, 2)
 end
 
 function love.resize(w, h)
@@ -86,32 +95,36 @@ function love.keypressed(key)
     end
 
     if key == "return" then
-        anim.frames[anim.frame] = newframe()
+        anim.frames[editor.frame] = newframe()
     end
 
     if key == "backspace" then
-        table.remove(anim.frames, anim.frame)
-        if anim.frame > 1 then
-            anim.frame = anim.frame - 1
+        if #anim.frames == 1 then
+            anim.frames[editor.frame] = newframe()
+            return
+        end
+        table.remove(anim.frames, editor.frame)
+        if editor.frame > 1 then
+            editor.frame = editor.frame - 1
         end
     end
 
     if key == "space" then
-        anim.playing = not anim.playing
-        if not anim.playing then anim.tick = 0 end
+        editor.playing = not editor.playing
+        if not editor.playing then editor.tick = 0 end
     end
 
     if key == "right" then
-        anim.frame = anim.frame + 1
-        if not anim.frames[anim.frame] then
-            anim.frames[anim.frame] = newframe()
+        editor.frame = editor.frame + 1
+        if not anim.frames[editor.frame] then
+            anim.frames[editor.frame] = newframe()
         end
-    elseif key == "left" and anim.frame > 1 then
-        anim.frame = anim.frame - 1
+    elseif key == "left" and editor.frame > 1 then
+        editor.frame = editor.frame - 1
     end
 
     if key == "o" then
-        settings.onion = not settings.onion
+        anim.onion = not anim.onion
     end
 end
 
---
+++ settings.lua
@@ -0,0 +1,5 @@
+return {
+    bg = { 0, 0, 0 }, -- background color
+    scale = 2, -- editor scaling
+    nearest = true,
+}
