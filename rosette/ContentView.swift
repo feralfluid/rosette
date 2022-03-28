19913e3c1b7205bb2624121bdb561d40743ffe66
Ty Poorman <github@flotts.io>
2022-03-27 23:23:27 -0400

    Initial Commit

---
+++ rosette/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  rosette
+//
+//  Created by Ty Poorman on 3/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ rosette/rosetteApp.swift
@@ -0,0 +1,17 @@
+//
+//  rosetteApp.swift
+//  rosette
+//
+//  Created by Ty Poorman on 3/27/22.
+//
+
+import SwiftUI
+
+@main
+struct rosetteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
